4f50d267014c365aac8d8bffca3047b7cfc0ee16
freyacooper <138793044+freyacooper@users.noreply.github.com>
2023-10-17 07:24:12 +0200

    fonction fetch pour afficher Gallery

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,25 +1,49 @@
+let list
 
-const listWorks = fetch("http://localhost:5678/api/works").then(listWorks => listWorks.json())
-console.log(listWorks)
-
-function afficherGallery(listWorks) {
-    for(let i = 0; i < listWorks.length; i++) {
-            const projet = listWorks[i]
-            console.log(listWorks[i])
-            const divGallery = document.querySelector(".gallery")
-            const figure = document.createElement("figure")
-
-            const img = document.createElement("img")
-            img.src = projet.imageUrl
-
-            const caption = document.createElement("figcaption")
-            caption.innnerText = projet.title
-
-            figure.appendChild(img)
-            figure.appendChild(caption)
-
-            divGallery.appendChild(figure)
-        }
+async function fetchWorks() {
+    const response = await fetch("http://localhost:5678/api/works")
+    list = await response.json()
+    return list
 }
 
-afficherGallery(listWorks)
+fetchWorks().then(list => {
+    
+    for(let i = 0; i < list.length; i++) {
+        const projet = list[i]
+        const divGallery = document.querySelector(".gallery")
+        const figure = document.createElement("figure")
+
+        const img = document.createElement("img")
+        img.src = projet.imageUrl
+
+        const caption = document.createElement("figcaption")
+        caption.innerText = projet.title
+
+        figure.appendChild(img)
+        figure.appendChild(caption)
+
+        divGallery.appendChild(figure)
+    }
+})
+
+// function afficherGallery(list) {
+//     for(let i = 0; i < list.length; i++) {
+//             const projet = list[i]
+//             console.log(list[i])
+//             const divGallery = document.querySelector(".gallery")
+//             const figure = document.createElement("figure")
+
+//             const img = document.createElement("img")
+//             img.src = projet.imageUrl
+
+//             const caption = document.createElement("figcaption")
+//             caption.innnerText = projet.title
+
+//             figure.appendChild(img)
+//             figure.appendChild(caption)
+
+//             divGallery.appendChild(figure)
+//         }
+// }
+
+// afficherGallery(list)
