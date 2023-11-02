fd6dcc02fc2f80e0c58a1aeac0cf4be124e73b15
freyacooper <138793044+freyacooper@users.noreply.github.com>
2023-11-02 16:31:02 +0100

    modal cree et suppression des travaux

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -1,3 +1,28 @@
+const divGalleryMod = document.createElement("div")
+divGalleryMod.classList.add("gallery-mod")
+
+function afficherGallery(list) {
+    for(let i = 0; i < list.length; i++) {
+        const projet = list[i]
+        
+        const divImage = document.createElement("div")
+        divImage.classList.add("div-image")
+        divImage.setAttribute("data-id", `${projet.id}`)
+
+        const img = document.createElement("img")
+        img.src = projet.imageUrl
+        
+        const iconPoubelle = document.createElement("i")
+        iconPoubelle.classList.add("fa-solid", "fa-trash-can", "fa-lg")
+        iconPoubelle.setAttribute("data-id", `${projet.id}`)
+
+        divImage.appendChild(img)
+        divGalleryMod.appendChild(divImage)
+        divImage.appendChild(iconPoubelle)
+        modalBox.insertBefore(divGalleryMod, divLine)
+    }
+}
+
 const modal = document.createElement("div")
 modal.classList.add("modal")
 const modalBox = document.createElement("div")
@@ -12,8 +37,23 @@ modalHeader.appendChild(cross)
 const modalTitre = document.createElement("h3")
 modalTitre.innerText = "Galerie photo"
 
+const divLine = document.createElement("div")
+divLine.classList.add("line")
+
+const boutonAjouter = document.createElement("a")
+boutonAjouter.innerText = "Ajouter une photo"
+
+fetch("http://localhost:5678/api/works").then(res => {
+    return res.json()
+})
+.then(list => {
+    afficherGallery(list)
+})
+
 modalBox.appendChild(modalHeader)
 modalBox.appendChild(modalTitre)
+modalBox.appendChild(divLine)
+modalBox.appendChild(boutonAjouter)
 modal.appendChild(modalBox)
 
 const barreEdition = document.querySelector(".barre-edition")
@@ -22,6 +62,26 @@ body.insertBefore(modal, barreEdition)
 const boutonModifier = document.querySelector(".modifier")
 boutonModifier.addEventListener("click", () =>{
     modal.style.display = "block"
+
+    const listIcons = document.querySelectorAll(".div-image i")
+    listIcons.forEach(icon => {
+        icon.addEventListener("click", (event) => {
+
+            event.preventDefault()
+            const imgSupprime = document.querySelector(`div[data-id="${event.target.dataset.id}"]`)
+            const token = localStorage.getItem("token")
+           
+            fetch(`http://localhost:5678/api/works/${event.target.dataset.id}`, {
+                method: "DELETE",
+                headers: { "Authorization" : `Bearer ${token}` },
+            })
+            .then(res => {
+                if(res.status === 200) {
+                    imgSupprime.style.display = "none"
+                }
+            })
+        })
+})
 })
 
 modal.addEventListener("click", (event) => {
